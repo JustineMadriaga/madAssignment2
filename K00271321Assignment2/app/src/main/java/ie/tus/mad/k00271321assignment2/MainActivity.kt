377ba59115663b3bec3c55854c1850e0be148074
JustineM424 <K00271321@student.tus.ie>
2024-12-06 12:28:08 +0000

    Setup

---
+++ K00271321Assignment2/app/src/main/java/ie/tus/mad/k00271321assignment2/MainActivity.kt
@@ -0,0 +1,47 @@
+package ie.tus.mad.k00271321assignment2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ie.tus.mad.k00271321assignment2.ui.theme.K00271321Assignment2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            K00271321Assignment2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    K00271321Assignment2Theme {
+        Greeting("Android")
+    }
+}
